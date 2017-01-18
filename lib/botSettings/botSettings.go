b608e0a2849127d62ea11ba5d378a4fa178a85c5
Amaury Louarn <git@homnomnom.fr>
2017-01-18 11:14:18 +0100

    added bot settings

---
+++ lib/botSettings/botSettings.go
@@ -0,0 +1,37 @@
+package botSettings
+
+import (
+    "encoding/json"
+    "errors"
+    "log"
+    "os"
+)
+
+type Settings struct {
+    Token string
+    Channel string
+    DefaultLocation string
+}
+
+func setError(err error) error {
+    return errors.New("BotSetting: " + err.Error())
+}
+
+func GetSettings(filename string) (Settings, error) {
+    file, err := os.Open(filename)
+    if err != nil {
+        localError := setError(err)
+        log.Fatal(localError)
+        return Settings{}, localError
+    }
+    dec := json.NewDecoder(file)
+
+    var settings Settings
+    if err := dec.Decode(&settings); err != nil {
+        localError := setError(err)
+        log.Fatal(localError)
+        return Settings{}, localError
+    }
+
+    return settings, nil
+}
