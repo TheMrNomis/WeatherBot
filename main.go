88f35d64d0fd99cab45785d89899c623853ea4b6
Amaury Louarn <git@homnomnom.fr>
2017-01-27 09:26:40 +0100

    install command-line

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
     "fmt"
     "os"
     "os/signal"
+    "flag"
 
     "github.com/bwmarrin/discordgo"
     "weatherbot/lib/botSettings"
     "weatherbot/lib/weather"
+    "weatherbot/lib/install"
 )
 
 var m_settings botSettings.Settings
+var m_install bool
 
 func init() {
     settings, err := botSettings.GetSettings("settings.json")
@@ -21,39 +24,53 @@ func init() {
 
     m_settings = settings
     weather.Init(&settings)
+
+    flag.BoolVar(&m_install, "install", false, "--install if you want to install the db")
+
+    flag.Parse()
 }
 
 func main() {
-    defer weather.Close()
-    if m_settings.Token == "" || m_settings.Token == "<your token>" {
-        log.Fatal("no token provided")
-        return
+    if m_install {
+        log.Println("installing")
+
+        db, err := weather.OpenDatabase(&m_settings)
+        if err != nil {
+            log.Fatal(err)
+        }
+        install.CreateCityDb(db, "city.list.json")
+    } else {
+        defer weather.Close()
+        if m_settings.Token == "" || m_settings.Token == "<your token>" {
+            log.Fatal("no token provided")
+            return
+        }
+
+        discord, err := discordgo.New(m_settings.Token)
+        if err != nil {
+            log.Fatal("Error creating Discord session: ", err)
+            return
+        }
+
+        discord.AddHandler(weather.HandleReady)
+        discord.AddHandler(weather.HandleGuildCreate)
+        discord.AddHandler(weather.HandleChannelCreate)
+        discord.AddHandler(weather.HandleMessage)
+
+        err = discord.Open()
+        if err != nil {
+            log.Fatal("Error opening Discord session: ", err)
+        }
+
+        log.Println("WeatherBot is running, ^C to exit")
+        sigchan := make(chan os.Signal, 10)
+        signal.Notify(sigchan, os.Interrupt)
+        <-sigchan
+        fmt.Println()
+        log.Println("Program killed")
+
+        discord.Close()
+
+        os.Exit(0)
     }
-
-    discord, err := discordgo.New(m_settings.Token)
-    if err != nil {
-        log.Fatal("Error creating Discord session: ", err)
-        return
-    }
-
-    discord.AddHandler(weather.HandleReady)
-    discord.AddHandler(weather.HandleGuildCreate)
-    discord.AddHandler(weather.HandleChannelCreate)
-    discord.AddHandler(weather.HandleMessage)
-
-    err = discord.Open()
-    if err != nil {
-        log.Fatal("Error opening Discord session: ", err)
-    }
-
-    log.Println("WeatherBot is running, ^C to exit")
-    sigchan := make(chan os.Signal, 10)
-    signal.Notify(sigchan, os.Interrupt)
-    <-sigchan
-    fmt.Println()
-    log.Println("Program killed")
-
-    discord.Close()
-
-    os.Exit(0)
 }
